634d80aaf9da14e209db4be739fe885a99723835
Rasul <73281386+Rassska@users.noreply.github.com>
2021-08-15 17:08:09 +0300

    Time: 0 ms (100.00%), Space: 10.7 MB (28.92%) - LeetHub

--- remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -11,19 +11,40 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+       
+        // 1) Claryfying the problem
+            // 1.1) 1 <= list.size() <= 30
+            // 1.2) 0 <= Node.val <= 100
+            // 1.3) integerer numbers
+            // 1.4) returning pointer to the head after removing n-th element
         
-        ListNode* fast = head, *slow = head;
+        // 2) Initial approaches
+            // 2.1) making another pointer to that linked list, reversing, removing n-th element
+            // 2.2) making two pointers, first is iterating n times, till first -> next != nullptr, we move the second pointer.
+            // 2.2.2) and changing the second -> next = second -> next -> next;
         
-        while(n--) fast = fast->next;
+        // 3) Writing the solution
         
-        if (!fast) return head->next;
+            ListNode* first = head;
+            ListNode* second = head;
         
-        while(fast->next) {
-            fast = fast->next; 
-            slow = slow->next;
+        while(n--) {
+            first = first -> next;
+        } 
+        
+        if (first == nullptr) return head -> next;
+        while (first -> next != nullptr) {
+            first = first -> next;
+            second = second -> next;
         }
-        slow->next = slow->next->next;
+        
+        second -> next = second -> next -> next;
+        
         return head;
+        
+        // 4) Debugging 
+            // 4.1) head = [1,2,3,4,5], n = 2  => first -> val = 3, second -> val = 3;
+            // 4.2) head = [1], n = 1 => 
         
         
         
