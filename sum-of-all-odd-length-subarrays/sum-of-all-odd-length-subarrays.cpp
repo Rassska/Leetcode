61f9a023ca046445ae6c53c67e29b011331ada06
Rasul <73281386+Rassska@users.noreply.github.com>
2021-09-07 10:57:29 +0300

    Time: 4 ms (66.19%), Space: 8.5 MB (14.56%) - LeetHub

--- sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.cpp
+++ sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.cpp
@@ -4,17 +4,20 @@ public:
        
         
         int step = 1;
-        std::size_t globalSum = 0;
+        int globalSum = 0;
+        std::vector<int> prefixSum(arr.size() + 1, 0);
         
-        while (step <= arr.size()) {
-            std::size_t localSum = 0;
+        for (std::size_t i = 0; i < arr.size(); i++) {
+            prefixSum[i + 1] = arr[i] + prefixSum[i];
+        }
+        
+        while (step <= arr.size()) { // O(arr.size())
+            int localSum = 0;
+            int r = step;
+            int l = 0;
             
-            for(std::size_t i = 0; i < arr.size() - step + 1; i++) {
-                std::size_t currArrSum = 0;
-                for(std::size_t j = i; j < i + step; j++) {
-                    currArrSum += arr[j];
-                }
-                localSum += currArrSum;
+            while (r <= arr.size()) {
+                localSum += prefixSum[r++] - prefixSum[l++];
             }
             globalSum += localSum;
             
@@ -30,6 +33,17 @@ public:
                 step = 3: localSum = 7 + 11 + 10; globalSum = 28
                 step = 5: 
         
+            *****************
+            Time complexity - O(arr.size() * arr.size() * arr.size()) => O(arr.size()^3)
+            Space complexity - O(1)
+            
+            prefixSum[i] = (sum[0]...sum[i]);
+            
+            arr = [1,4,2,5,3]
+            *****************
+            step = 1: localSum = 1 + 4 + 2 + 5
+            
+            
         */
         
         
