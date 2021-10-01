a6407f3cc925521cfc062f7e35a21240c3ca82ed
Rasul <73281386+Rassska@users.noreply.github.com>
2021-10-01 13:25:23 +0300

    Time: 120 ms (58.42%), Space: 93.3 MB (53.76%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -2,40 +2,18 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         
+    // minSoFar - min{prices[0]...prices[i]}
         
-        // 1) Claryfying the problem
-            // 1.1) 1 <= prices.size() <= 10^5
-            // 1.2) 0 <= prices[i] <= 10^4
-            // 1.3) returning max profit
+    // dp[i] = prices[i] - minSoFar
         
-        
-        // 2) Initial approaches
-            // 2.1) brute force
-        
-        
-        // 3) Solution
-        
-        int maxProfit = 0;
-        int currMin = std::numeric_limits<int>::max();
-        std::vector<int> dp(prices.size());
-        
-        for (std::size_t i = 0; i < prices.size(); i++) {
-            currMin = min (currMin, prices[i]);
-            dp[i] = prices[i] - currMin; 
+    
+        int minSoFar = prices[0];
+        int ans = 0;
+        for (std::size_t i = 1; i < prices.size(); i++) {
+            ans = std::max(ans, prices[i] - minSoFar);
+            minSoFar = std::min(minSoFar, prices[i]);
         }
-        
-        
-        return *max_element(dp.begin(), dp.end());
-        
-        // 4) Debugging
-            // 4.1) [7, 1, 5, 3, 6, 4] 
-        
-        // 5) Analyzing
-            // 5.1) Time complexity - O((prices.size())^2)
-            // 5.2) Space complexity - O(1)
-        
-        
-        
-        
+    
+        return ans;
     }
 };
