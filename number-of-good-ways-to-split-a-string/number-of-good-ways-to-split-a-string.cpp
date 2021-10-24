37afb6f45605e53545396b2cd5fc6725bb593043
Rasul <73281386+Rassska@users.noreply.github.com>
2021-10-24 16:00:48 +0300

    Time: 59 ms (24.54%), Space: 8.5 MB (61.28%) - LeetHub

--- number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.cpp
+++ number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.cpp
@@ -2,53 +2,26 @@ class Solution {
 public:
     int numSplits(string s) {
         
-        std::vector<std::vector<std::size_t>> leftPref(maxen, std::vector<std::size_t>(s.size() + 1, 0));
-        std::vector<std::vector<std::size_t>> rightPref(maxen, std::vector<std::size_t>(s.size() + 1, 0));
-        std::size_t ans = 0;
-        
+        std::set<char> leftPref;
+        std::unordered_map<char, int> hashMap;
+        int ans = 0;
         for (std::size_t i = 0; i < s.size(); i++) {
-            for (std::size_t j = 0; j < 26; j++) {
-                if (j == s[i] - 'a') {
-                    leftPref[s[i] - 'a'][i + 1] = leftPref[s[i] - 'a'][i] + 1;
-                } else {
-                    leftPref[j][i + 1] = leftPref[j][i];
-                }
-                
-            }
-            
-        }
-        
-        std::size_t rightPrefPointer = 0;
-        
-        for (int i = s.size() - 1; i >= 0; i--) {
-            for (std::size_t j = 0; j < 26; j++) {
-                if (j == s[i] - 'a') {
-                    rightPref[s[i] - 'a'][rightPrefPointer + 1] = rightPref[s[i] - 'a'][rightPrefPointer] + 1;
-                   
-                } else {
-                    rightPref[j][rightPrefPointer + 1] = rightPref[j][rightPrefPointer];
-                }
-                
-            }
-            rightPrefPointer++;
+            hashMap[s[i]]++;
         }
         
         for (std::size_t i = 0; i < s.size(); i++) {
-            std::size_t nonZerroCharsAmountLeftPref = 0;
-            std::size_t nonZerroCharsAmountRightPref = 0;
-            for (std::size_t j = 0; j < 26; j++) {
-                if (leftPref[j][i + 1] != 0) {
-                    nonZerroCharsAmountLeftPref++;
-                }
-            }
-            for (std::size_t j = 0; j < 26; j++) {
-                if (rightPref[j][s.size() - i - 1] != 0) {
-                    nonZerroCharsAmountRightPref++;
-                }
+            leftPref.insert(s[i]);
+            
+            if (hashMap[s[i]] == 1) {
+                hashMap.erase(s[i]);
+            } else {
+                hashMap[s[i]]--;
             }
             
-            if (nonZerroCharsAmountRightPref == nonZerroCharsAmountLeftPref) 
+            if (leftPref.size() == hashMap.size()) {
                 ans++;
+            }
+            
         }
         return ans;
         
