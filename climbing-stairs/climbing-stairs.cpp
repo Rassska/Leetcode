aa1dd786ce3b08c9271ce6900b54eae9ef9033bc
Rasul <73281386+Rassska@users.noreply.github.com>
2021-10-26 18:08:33 +0300

    Time: 0 ms (100.00%), Space: 6.2 MB (26.51%) - LeetHub

--- climbing-stairs/climbing-stairs.cpp
+++ climbing-stairs/climbing-stairs.cpp
@@ -4,18 +4,17 @@ public:
         
         if (n == 1) {
             return 1;
-        } else if (n == 2) {
-            return 2;
         }
-        
         std::vector<int> dp(n + 1);
-        dp[0] = 0;
+        
         dp[1] = 1;
         dp[2] = 2;
         
-        for (std::size_t i = 3; i <= n; i++) {
+        for (std::size_t i = 3; i < n + 1; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
+        
+        
         return dp[n];
     }
 };
