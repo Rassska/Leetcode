5f5087dff26873480147ba0331e2f47ebcbb5586
Rasul <73281386+Rassska@users.noreply.github.com>
2021-10-26 17:28:00 +0300

    Time: 0 ms (100.00%), Space: 9.6 MB (6.02%) - LeetHub

--- search-a-2d-matrix/search-a-2d-matrix.cpp
+++ search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -2,27 +2,24 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
-        
-        /*
-            1) Integers in each row are sorted from left to right.
-            2) The first integer of each row is greater than the last integer of the previous row.
-        
-        
-        */
-        
-        std::vector<int> nums;
-        
         for (std::size_t i = 0; i < matrix.size(); i++) {
-            for (std::size_t j = 0; j < matrix[i].size(); j++) {
-                nums.push_back(matrix[i][j]);
-            }
-        }
-        
-        for (std::size_t i = 0; i < nums.size(); i++) {
-            if (nums[i] == target) {
-                return true;
+            int left = 0;
+            int right = matrix[i].size() - 1;
+            
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                
+                if (matrix[i][mid] == target) {
+                    return true;
+                } else if (matrix[i][mid] > target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
             }
         }
         return false;
+        
+        
     }
 };
