535ed07eece62047dbef65640cf8d3e73cede81d
Rasul <73281386+Rassska@users.noreply.github.com>
2021-10-26 12:12:41 +0300

    Time: 4 ms (95.65%), Space: 13.5 MB (85.18%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,48 +1,54 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
+    
+    
+    int findBound (std::vector<int>& nums, int target, bool isFirst) {
         
-        
-        
-        // 1) Claryfying the problem
-            // 1.1) -10^9 <= nums[i] <= 10^9 , for target the same constraints
-            // 1.2) 0 <= nums.size() <= 10^5
-            // 1.3) return first and last entry indices of target or <-1, -1> if target doesn't exist
-        
-        // 2) Initial approaches
-            // 2.1) brute force => finding the first and the last entry points. O(n) - time compl, O(1) - space
-            // 2.2) binary search the first entry index of target and the last one separately. O(log n) - time compl, O(1) - space
-        
-        // 3) Solution
-        
-        int left = 0, right = nums.size(), first = -1, last = -1;
-        
-        while (left < right) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
             int mid = (left + right) / 2;
             
-            if (nums[mid] >= target) {
-                right = mid;
+            if (nums[mid] == target) {
+                if (isFirst) {
+                    if (mid == left || nums[mid - 1] != target) {
+                        return mid;
+                    } 
+                    
+                    right = mid - 1;
+                    
+                } else {
+                    if (mid == right || nums[mid + 1] != target) {
+                        return mid;
+                    }
+                    left = mid + 1;
+                } 
+            } else if (nums[mid] > target) {
+                right = mid - 1;
             } else {
                 left = mid + 1;
             }
-        }
-        first = left;
-        left = 0;
-        right = nums.size();
-        
-        while (left < right) {
-            int mid = (right + left) / 2;
             
-            if (nums[mid] > target) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
         }
-        last = left;
+        return -1;
         
-        return last == first ? std::vector<int>{-1, -1} : std::vector<int>{first, last - 1};
+        
         
     }
     
+    
+    vector<int> searchRange(vector<int>& nums, int target) {
+        
+        std::vector<int> ans = {-1, -1};
+        if (!nums.size()) {
+            return ans;
+        } else {
+            ans[0] = findBound(nums, target, true);
+            ans[1] = findBound(nums, target, false);
+        }
+        
+        return ans;
+        
+    }
 };
