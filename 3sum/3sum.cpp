6bc1ad29c1078a16dbe2f82f7967da5d60265bf6
Rasul <73281386+Rassska@users.noreply.github.com>
2021-10-28 12:17:49 +0300

    Time: 76 ms (79.08%), Space: 21.3 MB (37.67%) - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -3,34 +3,41 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         
         std::vector<std::vector<int>> ans;
+        if (nums.size() < 3) {
+            return ans;
+        }
+        
         std::sort(nums.begin(), nums.end());
         
-        for (std::size_t i = 0; i < nums.size(); i++) {
-            int target = -nums[i];
+        // nums = {-4, -1, -1, 0, 1, 2}
+        for (std::size_t i = 0; i < nums.size() - 1; i++) {
+            
+            int first = nums[i];
+            int target = -1 * first;
             int left = i + 1;
             int right = nums.size() - 1;
             
+            
             while (left < right) {
-                int sum = nums[left] + nums[right];
                 
-                if (sum < target) {
+                if (nums[left] + nums[right] < target) {
                     left++;
-                } else if (sum > target) {
+                } else if (nums[left] + nums[right] > target) {
                     right--;
                 } else {
+                    std::vector<int> currTriplet = {first, nums[left], nums[right]};
+                    ans.push_back(currTriplet);
                     
-                    std::vector<int> tempAns = {-target, nums[left], nums[right]};
-                    ans.push_back(tempAns);
-                    
-                    while (left < right && nums[left] == tempAns[1]) left++;
-                    while (left < right && nums[right] == tempAns[2]) right--;
+                    while (left < right && nums[left] == currTriplet[1]) left++;
+                    while (left < right && nums[right] == currTriplet[2]) right--;
                 }
             }
-            
-            while (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
+            while (i + 1 < nums.size() && nums[i + 1] == nums[i]) {
                 i++;
             }
+            
         }
+        
         return ans;
         
     }
