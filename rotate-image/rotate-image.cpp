f8562b1a17aa4a3f925f6fa617b1f35060b55de9
Rasul <73281386+Rassska@users.noreply.github.com>
2021-10-28 15:26:56 +0300

    Time: 4 ms (51.12%), Space: 7.1 MB (73.73%) - LeetHub

--- rotate-image/rotate-image.cpp
+++ rotate-image/rotate-image.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int matrixSideLength = matrix[0].size();
         
-        for (std::size_t i = 0; i < matrixSideLength; i++) {
-            for (std::size_t j = 0; j < i; j++) {
-                swap(matrix[j][i], matrix[i][j]);
+        
+        /*
+            [[1,2,3],
+             [4,5,6],
+             [7,8,9]]
+                
+            [[1, 4, 7]
+             [2, 5, 8]
+             [3, 6, 9]]
+        
+        */
+        
+        
+        for (std::size_t i = 0; i < matrix.size(); i++) {
+            for (std::size_t j = i; j < matrix[i].size(); j++) {
+                std::swap(matrix[i][j], matrix[j][i]);
             }
         }
         
-        for (std::size_t i = 0; i < matrixSideLength; i++) {
+        for (std::size_t i = 0; i < matrix.size(); i++) {
             std::reverse(matrix[i].begin(), matrix[i].end());
         }
-        
-        
-        
-        
-        
         
     }
 };
