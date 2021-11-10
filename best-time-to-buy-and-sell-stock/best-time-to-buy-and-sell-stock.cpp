8fb673a26e079f5efa1a2be5808e33419ed66e6d
Rasul <73281386+Rassska@users.noreply.github.com>
2021-11-10 14:16:04 +0300

    Time: 124 ms (53.33%), Space: 93.4 MB (52.88%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int leftMin = std::numeric_limits<int>::max();
         
-    // minSoFar - min{prices[0]...prices[i]}
-        
-    // dp[i] = prices[i] - minSoFar
-        
-    
-        int minSoFar = prices[0];
-        int ans = 0;
-        for (std::size_t i = 1; i < prices.size(); i++) {
-            ans = std::max(ans, prices[i] - minSoFar);
-            minSoFar = std::min(minSoFar, prices[i]);
+        for (std::size_t i = 0; i < prices.size(); i++) {
+            leftMin = std::min(leftMin, prices[i]);
+            profit = std::max(profit, prices[i] - leftMin);
         }
-    
-        return ans;
+        
+        return profit;
     }
 };
