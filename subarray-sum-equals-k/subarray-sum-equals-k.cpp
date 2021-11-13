1c67dae01637603f3f8e94615da4b08c266d37af
Rasul <73281386+Rassska@users.noreply.github.com>
2021-11-13 17:48:02 +0300

    Time: 135 ms (16.87%), Space: 42.1 MB (13.33%) - LeetHub

---
+++ subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        
+        std::unordered_map<int, int> seen = {{0, 1}};
+        
+        int count = 0, sum = 0;
+        
+        for (auto n: nums) {
+            sum += n;
+            
+            count += seen[sum - k];
+            
+            seen[sum]++;
+        }
+        return count;
+    }
+};
