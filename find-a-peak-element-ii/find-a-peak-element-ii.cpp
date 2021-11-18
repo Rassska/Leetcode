cfa66fc0c7082cffce018c0b960a15a3d6d1e69c
Rasul <73281386+Rassska@users.noreply.github.com>
2021-11-18 09:47:10 +0300

    Time: 160 ms (79.37%), Space: 46.1 MB (85.52%) - LeetHub

---
+++ find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        
+        int left = 0;
+        int right = mat[0].size();
+        int line = 0;
+        while (left < right) {
+            std::pair<int, int> mid = {0, left + (right - left) / 2};
+            int midValue = mat[mid.first][mid.second];
+            for (std::size_t i = 0; i < mat.size(); i++) {
+                if (mat[i][mid.second] > midValue) {
+                    mid.first = i;
+                    line = i;
+                    midValue = mat[mid.first][mid.second];
+                } 
+            }
+            
+            if (mid.second - 1 >= 0 && mat[mid.first][mid.second] < mat[mid.first][mid.second - 1]) {
+                right = mid.second;
+            } else if (mid.second + 1 < mat[mid.first].size() && mat[mid.first][mid.second] < mat[mid.first][mid.second + 1]) {
+                left = mid.second + 1;
+            } else {
+                return {mid.first, mid.second};
+            }
+            
+        }
+        return {line, left};
+        
+        
+        
+    }
+};
