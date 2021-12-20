d7c6549f2dd2c4db6df70dc9ecedf896f79d4e09
Rasul <73281386+Rassska@users.noreply.github.com>
2021-12-20 18:03:30 +0300

    Time: 44 ms (97.43%), Space: 31.5 MB (38.97%) - LeetHub

--- missing-element-in-sorted-array/missing-element-in-sorted-array.cpp
+++ missing-element-in-sorted-array/missing-element-in-sorted-array.cpp
@@ -2,15 +2,44 @@ class Solution {
 public:
     int missingElement(vector<int>& nums, int k) {
         
+        int left = 0;
+        int right = nums.size() - 1;
         
-        for (std::size_t i = 1; i < nums.size(); i++) {
-            if (nums[i] - nums[i - 1] > k) {
-                return nums[i - 1] + k;
-            } else if (nums[i] - nums[i - 1] != 1 && nums[i] - nums[i - 1] <= k) {
-                k -= (nums[i] - nums[i - 1] - 1);
+        while (left < right) {
+            
+            int mid = left + (right - left) / 2;
+            if (right - left == 1 && nums[left] + k < nums[right]) {
+                return nums[left] + k;
+            } else if (right - left == 1) {
+                k -= nums[right] - nums[left] - 1;
+                break;
+            }
+            
+            if (((nums[mid] - nums[left]) - (mid - left)) >= k) {
+                right = mid;   
+            } else {
+                k -= ((nums[mid] - nums[left]) - (mid - left));
+                left = mid;
             }
         }
-        return nums[nums.size() - 1] + k;
+        return nums.back() + k;
+        
         
     }
 };
+
+
+
+// TC: O(g(n)), where n - nums.size()
+// SC: O(1)
+
+
+// nums = [4,7,9,10], k = 4
+// mid = 1, left = 0, right = 3; 
+// mid - left = 1
+// nums[mid] - nums[left] - (mid - left) < k
+//       
+
+
+// TC: O(g(log(n))), where n - nums.size()
+// SC: O(f(1))
