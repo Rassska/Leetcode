f7a11625c0cff0a20d1ebc6ea7c91b79ebc95353
Rasul <73281386+Rassska@users.noreply.github.com>
2021-12-21 02:30:10 +0300

    Time: 168 ms (52.09%), Space: 46.4 MB (20.26%) - LeetHub

--- find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -5,31 +5,30 @@ public:
         
         
         int left = 0;
-        int right = mat[0].size();
-        int line = 0;
-        while (left < right) {
-            std::pair<int, int> mid = {0, left + (right - left) / 2};
-            int midValue = mat[mid.first][mid.second];
+        int right = mat[0].size() - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int maxNumRowIndex = 0;
             
-            for (std::size_t i = 0; i < mat.size(); i++) {
-                if (mat[i][mid.second] > midValue) {
-                    mid.first = i;
-                    line = i;
-                    midValue = mat[mid.first][mid.second];
+            for (int i = 0; i < mat.size(); i++) {
+                if (mat[i][mid] > mat[maxNumRowIndex][mid]) {
+                    maxNumRowIndex = i;
                 }
             }
             
-            if (mid.second > 0 && mat[mid.first][mid.second] < mat[mid.first][mid.second - 1]) {
-                right = mid.second;
-            } else if (mid.second + 1 < mat[0].size() && mat[mid.first][mid.second] < mat[mid.first][mid.second + 1]) {
-                left = mid.second + 1;
+            if ((mid == 0 || mat[maxNumRowIndex][mid] > mat[maxNumRowIndex][mid - 1]) && (mid == mat[0].size() - 1 || mat[maxNumRowIndex][mid] > mat[maxNumRowIndex][mid + 1])) {
+                return std::vector<int>({maxNumRowIndex, mid});
+            } else if (mid > 0 && mat[maxNumRowIndex][mid] < mat[maxNumRowIndex][mid - 1]) {
+                right = mid - 1;
             } else {
-                return std::vector<int>({mid.first, mid.second});
+                left = mid + 1;
             }
 
         }
+        return std::vector<int>({-1, -1});
         
-        return std::vector<int>({line, left});
+      
      
     }
     
