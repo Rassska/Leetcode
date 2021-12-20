500a0fa04693033273ab43626f1493f21d904503
Rasul <73281386+Rassska@users.noreply.github.com>
2021-12-21 02:15:09 +0300

    Time: 164 ms (66.96%), Space: 46.3 MB (62.55%) - LeetHub

--- find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
+    
+        
         
         int left = 0;
         int right = mat[0].size();
@@ -8,26 +10,29 @@ public:
         while (left < right) {
             std::pair<int, int> mid = {0, left + (right - left) / 2};
             int midValue = mat[mid.first][mid.second];
+            
             for (std::size_t i = 0; i < mat.size(); i++) {
                 if (mat[i][mid.second] > midValue) {
                     mid.first = i;
                     line = i;
                     midValue = mat[mid.first][mid.second];
-                } 
+                }
             }
             
-            if (mid.second - 1 >= 0 && mat[mid.first][mid.second] < mat[mid.first][mid.second - 1]) {
+            if (mid.second > 0 && mat[mid.first][mid.second] < mat[mid.first][mid.second - 1]) {
                 right = mid.second;
-            } else if (mid.second + 1 < mat[mid.first].size() && mat[mid.first][mid.second] < mat[mid.first][mid.second + 1]) {
+            } else if (mid.second + 1 < mat[0].size() && mat[mid.first][mid.second] < mat[mid.first][mid.second + 1]) {
                 left = mid.second + 1;
             } else {
-                return {mid.first, mid.second};
+                return std::vector<int>({mid.first, mid.second});
             }
-            
+
         }
-        return {line, left};
         
-        
-        
+        return std::vector<int>({line, left});
+     
     }
+    
 };
+
+
