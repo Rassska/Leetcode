34cb3f2d09e18d8bbe43184a24d0d3f81e41afc8
Rasul <73281386+Rassska@users.noreply.github.com>
2021-12-21 07:09:28 +0300

    Time: 0 ms (100.00%), Space: 6.2 MB (10.21%) - LeetHub

--- fibonacci-number/fibonacci-number.cpp
+++ fibonacci-number/fibonacci-number.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
-        } else if (n == 2) {
-            return 1;
-        }
-        std::vector<int> dp(n + 1);
+        
+        std::vector<int> dp(n + 5);
         dp[0] = 0;
+        
         dp[1] = 1;
         dp[2] = 1;
         
@@ -17,5 +12,6 @@ public:
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
+        
     }
 };
