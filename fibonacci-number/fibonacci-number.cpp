7b6c959a0f982903ed876833aad06c644c6ad0c2
Rasul <73281386+Rassska@users.noreply.github.com>
2021-12-21 07:26:20 +0300

    Time: 0 ms (100.00%), Space: 6.1 MB (18.56%) - LeetHub

--- fibonacci-number/fibonacci-number.cpp
+++ fibonacci-number/fibonacci-number.cpp
@@ -1,17 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
-        
-        std::vector<int> dp(n + 5);
-        dp[0] = 0;
-        
-        dp[1] = 1;
-        dp[2] = 1;
-        
-        for (std::size_t i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        return dp[n];
+       
+        double goldenRatio = (1 + sqrt(5)) / 2;
+        return std::round(std::pow(goldenRatio, n) / sqrt(5));
         
     }
 };
