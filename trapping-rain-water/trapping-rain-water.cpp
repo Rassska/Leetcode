39318f761d461f7bfcc245a6716472c2fce34f95
Rasul <73281386+Rassska@users.noreply.github.com>
2021-12-26 09:15:33 +0300

    Time: 8 ms (79.11%), Space: 16.1 MB (15.25%) - LeetHub

--- trapping-rain-water/trapping-rain-water.cpp
+++ trapping-rain-water/trapping-rain-water.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        
-        std::vector<int> dp(height.size());
-        int currMaxHeight = std::numeric_limits<int>::min();
         int ans = 0;
+    
+        std::vector<int> preff(height.size() + 1), suff(height.size() + 1);
+        preff[0] = 0;
+        suff[0] = 0;
         
-        for (int i = 0; i < height.size(); i++) {
-            currMaxHeight = std::max(currMaxHeight, height[i]);
-            dp[i] = currMaxHeight - height[i];
+        for (std::size_t i = 0; i < height.size(); i++) {
+            preff[i + 1] = std::max(preff[i], height[i]);
+        }
+        int tempSuffPointer = 0;
+        for (int i = height.size() - 1; i >= 0; i--) {
+            suff[tempSuffPointer + 1] = std::max(suff[tempSuffPointer], height[i]);
+            tempSuffPointer++;
         }
         
-        currMaxHeight = -1;
         
-        for (int i = height.size() - 1; i > -1; i--) {
-            currMaxHeight = std::max(currMaxHeight, height[i]);
-            ans += std::min(currMaxHeight - height[i], dp[i]);
+        
+        for (std::size_t i = 0; i < height.size(); i++) {
+            int left_max = preff[i + 1];
+            int right_max = suff[height.size() - i];
+            
+           ans += std::min(right_max, left_max) - height[i]; // -1
         }
         return ans;
-        
     }
 };
