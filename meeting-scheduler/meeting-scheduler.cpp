fbafac4f58c6adad613cd3911434e5a5c4c9fbac
Rasul <73281386+Rassska@users.noreply.github.com>
2021-12-26 06:33:24 +0300

    Time: 203 ms (29.23%), Space: 40.3 MB (66.73%) - LeetHub

--- meeting-scheduler/meeting-scheduler.cpp
+++ meeting-scheduler/meeting-scheduler.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     vector<int> minAvailableDuration(vector<vector<int>>& slots1, vector<vector<int>>& slots2, int duration) {
         
-        uint32_t firstP = 0, secondP = 0;
-        std::vector<int> availableInterval;
+        std::vector<int> ans;
+        
         std::sort(slots1.begin(), slots1.end());
         std::sort(slots2.begin(), slots2.end());
+        
+        std::size_t firstP = 0, secondP = 0;
+        
         while (firstP < slots1.size() && secondP < slots2.size()) {
-            if (slots1[firstP][0] >= slots2[secondP][0] 
-                && slots1[firstP][0] + duration <= slots2[secondP][1] 
-                && slots1[firstP][0] + duration <= slots1[firstP][1]){
-                
-                availableInterval.push_back(slots1[firstP][0]);
-                availableInterval.push_back(slots1[firstP][0] + duration);
+            // slots1[firstP][0] - start
+            // slots1[firstP][1] - end
+            
+            int currCompStart = std::max(slots1[firstP][0], slots2[secondP][0]); // 10
+            int currCompEnd = std::min(slots1[firstP][1], slots2[secondP][1]); // 15
+            
+            if (currCompStart + duration <= currCompEnd) {
+                ans.push_back(currCompStart);
+                ans.push_back(currCompStart + duration);
                 break;
-            } else if (slots1[firstP][0] < slots2[secondP][0] 
-                       && slots2[secondP][0] + duration <= slots1[firstP][1] 
-                       && slots2[secondP][0] + duration <= slots2[secondP][1]) {
                 
-                availableInterval.push_back(slots2[secondP][0]);
-                availableInterval.push_back(slots2[secondP][0] + duration);
-                break;
             }
             
             if (slots1[firstP][1] >= slots2[secondP][1]) {
@@ -30,8 +30,6 @@ public:
             }
         }
         
-        return availableInterval;
-        
-        
+        return ans;
     }
 };
