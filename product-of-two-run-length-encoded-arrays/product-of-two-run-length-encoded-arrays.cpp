cf1fd6e05eeec2fff6aa4c58795eb6f9ddf57c63
Rasul <73281386+Rassska@users.noreply.github.com>
2021-12-27 11:00:06 +0300

    Time: 544 ms (56.87%), Space: 204.5 MB (54.58%) - LeetHub

--- product-of-two-run-length-encoded-arrays/product-of-two-run-length-encoded-arrays.cpp
+++ product-of-two-run-length-encoded-arrays/product-of-two-run-length-encoded-arrays.cpp
@@ -3,50 +3,37 @@ public:
     vector<vector<int>> findRLEArray(vector<vector<int>>& encoded1, vector<vector<int>>& encoded2) {
         
         
-        auto mergeIntervals = [&](std::vector<std::vector<int>>& ans) {
-            std::vector<std::vector<int>> mergedAns;
-      
-            
-            for (std::size_t i = 0; i < ans.size(); i++) {
-                while(i < ans.size() - 1 && ans[i + 1][0] == ans[i][0]) {
-                    ans[i + 1][1] += ans[i][1];
-                    i++;
-                }
-                mergedAns.push_back(ans[i]);
-            }
-            
-            return mergedAns;
-        };
         
         int firstP = 0;
         int secondP = 0;
+        int prevProduct = 0;
+        int currProduct = 0;
         std::vector<std::vector<int>> ans;
         
         while (firstP < encoded1.size() && secondP < encoded2.size()) {
             std::vector<int> currPair;
             
             int currPairEndMin = std::min(encoded1[firstP][1], encoded2[secondP][1]); // 3
+            currProduct = encoded1[firstP][0] * encoded2[secondP][0];
             
-            currPair.push_back(encoded1[firstP][0] * encoded2[secondP][0]);
-            currPair.push_back(currPairEndMin);
-            ans.push_back(currPair);
             
-            if (encoded1[firstP][1] == currPairEndMin) {
-                firstP++;
+            if (ans.size() > 0 && currProduct == prevProduct) {
+                ans[ans.size() - 1][1] += currPairEndMin;
             } else {
-                encoded1[firstP][1] -= currPairEndMin;
+                ans.push_back({currProduct, currPairEndMin});
             }
             
-            if (encoded2[secondP][1] == currPairEndMin) {
-                secondP++;
-            } else {
-                encoded2[secondP][1] -= currPairEndMin;
-            }
+            prevProduct = currProduct;
+            
+            encoded1[firstP][1] -= currPairEndMin;
+            encoded2[secondP][1] -= currPairEndMin;
+            
+            if (encoded1[firstP][1] == 0) firstP++;
+            if (encoded2[secondP][1] == 0) secondP++;
             
             
         }
         
-        ans = mergeIntervals(ans);
         
         return ans;
         
