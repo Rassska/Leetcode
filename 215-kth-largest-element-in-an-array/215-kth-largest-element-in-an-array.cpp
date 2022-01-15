9a514e07f49881bcd0134cc93d99d25671565859
Rasul <73281386+Rassska@users.noreply.github.com>
2022-01-15 17:12:17 +0300

    Time: 7 ms (80.03%), Space: 10.1 MB (34.41%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     
-    int partition(vector<int> &mas, int l, int r) {
-        if (l != r)
-            std::swap(mas[l + rand() % (r - l)], mas[r]);
-        int x = mas[r];
-        int i = l - 1;
+    int partition(vector<int> &mas, int left, int right) {
+        if (left != right)
+            std::swap(mas[left + rand() % (right - left)], mas[right]);
         
-        for (int j = l; j <= r; j++) {
+        int x = mas[right]; // x = 4
+        int i = left - 1; // i = -1
+        
+        for (int j = left; j <= right; j++) {
             if (mas[j] <= x)
                 std::swap(mas[++i], mas[j]);
         }
-      return i;
+        return i;
     }
+    
     int nth(vector<int> mas, int n) {
         int l = 0, r = mas.size() - 1;
-        for(;;) {
+        while (true) {
             int pos = partition(mas,l,r);
             if (pos < n)
                 l = pos + 1;
