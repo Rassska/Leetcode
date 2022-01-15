0391409fb9e2b1e6cb8b698d4fe80922118ae3f5
Rasul <73281386+Rassska@users.noreply.github.com>
2022-01-15 19:22:10 +0300

    Time: 12 ms, Memory: 13.7 MB - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.cpp
+++ top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,29 +1,26 @@
+
+
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         
-        std::unordered_map <int, int> hashTable;
-        std::vector<std::vector<int>> buckets(nums.size() + 1);
-        std::vector<int> ans;
-        
-        for (int num : nums) {
-            hashTable[num]++;
+        for (auto& num : nums){
+            hashMap[num]++;
         }
         
-        for (std::pair<int, int> currPair : hashTable) {
-            buckets[currPair.second].push_back(currPair.first);
+        for (auto& currPair : hashMap) {
+            maxHeap.push(std::make_pair(currPair.second, currPair.first));
         }
         
-        for (std::size_t i = buckets.size() - 1; i != -1; i--) {
-            if (ans.size() < k) {
-                ans.insert(std::end(ans), std::begin(buckets[i]), std::end(buckets[i]));
-            } else {
-                break;
-            }
+        while (k--) {
+            ans.push_back(maxHeap.top().second);
+            maxHeap.pop();
         }
         return ans;
         
-        
-        
     }
+private:
+    std::unordered_map<int, int> hashMap;
+    std::priority_queue<std::pair<int, int>> maxHeap;
+    std::vector<int> ans;
 };
